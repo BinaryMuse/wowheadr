6cd139880af6f08454350c3425d5e47020e1ee9a
Brandon Tilley <binarymuse@binarymuse.net>
2010-11-28 01:47:53 -0800

    Add changes to URI:
    * URI.url now generates URLs for wowhead.com entities
    * URI.item, URI.spell, and URI.achievement use URI.url
    * All URI methods now take an optional subdomain parameter

--- test/wowheadr/uri.rb
+++ test/wowheadr/uri.rb
@@ -10,11 +10,31 @@ class URITest < Test::Unit::TestCase
     assert_equal Wowheadr::URI.item(12345), "http://www.wowhead.com/item=12345"
   end
 
+  def test_item_url_subdomain
+    assert_equal Wowheadr::URI.item(12345, 'ptr'), "http://ptr.wowhead.com/item=12345"
+  end
+
   def test_achievement_url
     assert_equal Wowheadr::URI.achievement(12345), "http://www.wowhead.com/achievement=12345"
   end
 
+  def test_achievement_url_subdomain
+    assert_equal Wowheadr::URI.achievement(12345, 'ptr'), "http://ptr.wowhead.com/achievement=12345"
+  end
+
   def test_spell_url
     assert_equal Wowheadr::URI.spell(12345), "http://www.wowhead.com/spell=12345"
+  end
+
+  def test_spell_url_subdomain
+    assert_equal Wowheadr::URI.spell(12345, 'ptr'), "http://ptr.wowhead.com/spell=12345"
+  end
+
+  def test_manual_url
+    assert_equal Wowheadr::URI.url(:npc, 48935), "http://www.wowhead.com/npc=48935"
+  end
+
+  def test_manual_url_subdomain
+    assert_equal Wowheadr::URI.url(:npc, 48935, 'ptr'), "http://ptr.wowhead.com/npc=48935"
   end
 end
